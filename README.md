70a018fb9b7c5100f6dd212c494e7bef704a8f0b
riceloya <109813509+riceloya@users.noreply.github.com>
2022-07-26 09:29:52 -0500

    "assignment for  July 26th- Ricardo Loya"

--- README.md
+++ README.md
@@ -26,6 +26,9 @@ per file. See the workflow for details.
 ### New Feature Request
 
 The color coding needs to be printed as a reference manual for wiring personnel.
+
 This manual is a mapping from the color-names to the corresponding numbers.
+
 Add a function that would format the color coding in a form that someone can print.
+
 Also resolve any coding guideline violations.
---
+++ test_func.py
@@ -0,0 +1,31 @@
+from get_color_func import *
+
+def test_number_to_pair(pair_number,
+                        expected_major_color, expected_minor_color):
+  major_color, minor_color = get_color_from_pair_number(pair_number)
+  assert(major_color == expected_major_color)
+  assert(minor_color == expected_minor_color)
+
+
+def test_pair_to_number(major_color, minor_color, expected_pair_number):
+  pair_number = get_pair_number_from_color(major_color, minor_color)
+  assert(pair_number == expected_pair_number)
+
+def print_color_code():
+  k = 1
+  with open('manual.txt','w') as redme:
+    for i in range(0,5):
+      for j in range(0,5):
+          pair_info = ['Pair number: ', str(k),' major color:',MAJOR_COLORS[i],' ',' minor_color:',MINOR_COLORS[j]]
+          with open('manual.txt','a') as redme:
+            redme.writelines(' '.join(map(str, pair_info)))
+            redme.writelines('\n')
+          print(pair_info)
+          k=k+1
+
+
+
+if __name__ == '__main__':
+  print('Color Code Manual:')
+  print_color_code()
+  print('Manual is done :)')
